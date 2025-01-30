09d4cfd2b98542a2e301fffb413318ba99372b73
LorenaIvanisevic <lorena.ivanisevic@fer.unizg.hr>
2025-01-30 18:52:46 +0100

    fix display

--- src/uvo-node.py
+++ src/uvo-node.py
@@ -22,6 +22,11 @@ class uvonode:
         # Sort them in the order of their distance
         matches = sorted(matches, key = lambda x:x.distance)
 
+
+        img3 = cv2.drawMatches(self.last_img,self.last_kp,img,kp,matches[:3],None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        cv2.imshow("Matched keypoints",img3)
+
+
         # Return best 3 matched keypoints
         pts1 = np.float32([self.last_kp[m.queryIdx].pt for m in matches[:3]])
         pts2 = np.float32([kp[m.trainIdx].pt for m in matches[:3]])
@@ -31,22 +36,17 @@ class uvonode:
 
 
     def process_image(self, cv_image):
-        # Process only every nth image
-        if self.count % self.pano_rate != 0: 
-            return
 
+        # Process only every nth image
+        if self.count % self.rate != 0: 
+            return
         
         keypoints, descriptors = self.orb.detectAndCompute(cv_image, None)
-
-        # Draw the keypoints on the image
-        # img_with_keypoints = cv2.drawKeypoints(cv_image, keypoints, None, color=(0, 255, 0), flags=0)
-        # cv2.imshow("img", img_with_keypoints)
-        # cv2.waitKey(3)
 
         pts1, pts2 = self.match_features(cv_image, keypoints, descriptors)
 
 
-        if (pts1 is not None and pts2 is not None):
+        if (pts1 is not None and pts2 is not None and len(pts1)==3 and len(pts2)==3):
 
             img_1 = self.last_img
             img_2 = cv_image
@@ -54,44 +54,18 @@ class uvonode:
             M = cv2.getAffineTransform(pts1, pts2)
             print ("Affine matrix:\n", M)
             
-            # get inverse
+            # Get inverse affine matrix
             #M_inv = np.linalg.inv( np.insert(M, 2,[0,0,1], axis=0) )
+       
+            cv2.imshow("Translated image", cv2.warpAffine(img_1, M, (img_1.shape[1], img_1.shape[0])))
 
-            if self.M is not None: 
-                #self.M = self.M @ M
-                self.M = M
-                print()
-            else:
-                self.M = M
-            
+            cv2.waitKey(3000)      
 
-            #warp_dst = cv2.warpAffine(img_1, self.M, (img_1.shape[1], img_1.shape[0]))
-
-            ## transform 2nd image
-           # img_2 = cv2.warpAffine(img_2, self.M[:2], (img_2.shape[1], img_2.shape[0]))
-
-            #result = cv2.addWeighted(self.pano, 0.5, inverse_transformed_image, 0.5, 0)
-            
-            #transformed_pano =  cv2.warpAffine(self.pano, M[:2], (self.pano.shape[1], self.pano.shape[0]))
-            
-            #result = cv2.addWeighted(transformed_pano, 0.5, img_2 , 0.5, 0)
-            #img_1[:img_2.shape[0], :img_2.shape[1]] = img_2
-            
-            #self.pano = img_1
-
-            cv2.imshow("Translated image", cv2.warpAffine(img_2, self.M, (img_2.shape[1], img_2.shape[0])))
-
-            cv2.waitKey(1000)      
-
-        
-    
         # Save information about previous frame
-        self.last_kp = keypoints
-        self.last_desc = descriptors
-        self.last_img = cv_image
-        self.pano_rate = 10
-        self.M = None
-
+        if (self.last_img is None or (len(pts1)==3 and len(pts2)==3)):
+            self.last_kp = keypoints
+            self.last_desc = descriptors
+            self.last_img = cv_image
         
 
     def callback(self, msg):
@@ -101,8 +75,6 @@ class uvonode:
 
             self.process_image(cv_image)
             self.count+=1        
-
-
 
         except Exception as e:
             rospy.logerr("Error processing image: %s", e)
@@ -144,11 +116,8 @@ class uvonode:
 
         self.sub = rospy.Subscriber(self.imageTopic, Image, self.callback)
 
-        self.imgs = []
         self.count = 0
-        self.pano_rate = 10
-        self.M = None
-        self.pano = None
+        self.rate = 10
 
  
 
