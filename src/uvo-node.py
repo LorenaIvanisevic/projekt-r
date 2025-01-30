b32af6f7b4242f2f356d807ca592bdb03e27dfa5
LorenaIvanisevic <lorena.ivanisevic@fer.unizg.hr>
2025-01-30 17:29:00 +0100

    in progress

--- src/uvo-node.py
+++ src/uvo-node.py
@@ -8,55 +8,125 @@ from geometry_msgs.msg import Pose
 import rosbag
 import rospkg
 import yaml
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 class uvonode: 
     def match_features(self, img, kp, desc):
-        #print(self.last_kp)
+
         if (self.last_img is None):
-        #if (self.last_kp is None or self.last_desc is None or len(self.last_kp)==0 or len(self.last_desc)==0):
             return None, None
         
         # Match descriptors
         matches = self.matcher.match(self.last_desc, desc)
 
-        # Sort them in the order of their distance, pick best 3
+        # Sort them in the order of their distance
         matches = sorted(matches, key = lambda x:x.distance)
     
         # Show best 10 matches
-        img3 = cv2.drawMatches(self.last_img, self.last_kp, img, kp,matches[:10], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-        cv2.imshow("img3",img3)
-        cv2.waitKey(3)
+        #img3 = cv2.drawMatches(self.last_img, self.last_kp, img, kp, matches[:10], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        #cv2.imshow("img3",img3)
+        #cv2.waitKey(1000)
 
-        # Return matched keypoints
+        # Return best 3 matched keypoints
         pts1 = np.float32([self.last_kp[m.queryIdx].pt for m in matches[:3]])
-        pts2 = np.float32([self.last_kp[m.trainIdx].pt for m in matches[:3]])
+        pts2 = np.float32([kp[m.trainIdx].pt for m in matches[:3]])
 
         return pts1, pts2
 
-    def publish_pose(R, t):
-        pose = Pose()
-
-
     def process_image(self, cv_image):
-        gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-
-        cv_image = cv2.undistort(cv_image, self.K, cv2.UMat(np.array(self.cam["distortion_coeffs"])))
+        # Process only every nth image
+        if self.count % self.pano_rate != 0: 
+            return  
+    
+        #cv_image = cv2.undistort(cv_image, self.K, cv2.UMat(np.array(self.cam["distortion_coeffs"])))
         
-        keypoints, descriptors = self.orb.detectAndCompute(gray_image, None)
+        keypoints, descriptors = self.orb.detectAndCompute(cv_image, None)
 
         # Draw the keypoints on the image
-        # img_with_keypoints = cv2.drawKeypoints(cv_image, keypoints, None, color=(0, 255, 0), flags=0)
+        img_with_keypoints = cv2.drawKeypoints(cv_image, keypoints, None, color=(0, 255, 0), flags=0)
+        cv2.imshow("img", img_with_keypoints)
+        cv2.waitKey(3)
 
         pts1, pts2 = self.match_features(cv_image, keypoints, descriptors)
 
+
         if (pts1 is not None and pts2 is not None):
-            src = pts1
-            dst = pts2
 
-            transform = cv2.getAffineTransform(src, dst)
+            if self.pano is None: 
+                self.pano =  cv2.copyMakeBorder(self.last_img, 100, 100, 100, 100, cv2.BORDER_CONSTANT, None, value=0)  # Black padding
+                #self.pano = self.last_img
 
-            print ("\nAffine matrix:\n",transform)
+            M = cv2.getAffineTransform(pts1, pts2)
+            
+            M_inv = np.linalg.inv( np.insert(M, 2,[0,0,1], axis=0) )
+
+            if self.M is not None: 
+                #self.M = self.M @ M
+                self.M = M
+                print()
+            else:
+                self.M = M
+            
+            #img_1 = self.last_img
+            img_1 = self.pano
+
+            ## add padding
+            img_2 = cv2.copyMakeBorder(cv_image, 100, 100, 100, 100, cv2.BORDER_CONSTANT, None, value=[0,0,255,0])  # transparent padding
+            #img_2 = cv_image
+
+            print(M_inv)
+
+            #warp_dst = cv2.warpAffine(img_1, self.M, (img_1.shape[1], img_1.shape[0]))
+
+            ## transform 2nd image
+            img_2 = cv2.warpAffine(img_2, self.M[:2], (img_2.shape[1], img_2.shape[0]))
+
+            #result = cv2.addWeighted(self.pano, 0.5, inverse_transformed_image, 0.5, 0)
+            
+            #transformed_pano =  cv2.warpAffine(self.pano, M[:2], (self.pano.shape[1], self.pano.shape[0]))
+            
+            #result = cv2.addWeighted(transformed_pano, 0.5, img_2 , 0.5, 0)
+            img_1[:img_2.shape[0], :img_2.shape[1]] = img_2
+            
+            self.pano = img_1
+
+            #cv2.imshow("Translated and Overlaid Images", self.pano)
+
+            #cv2.waitKey(3000)      
+
+            
+
+
+
+            ### below is stupid using stitching function
+
+            # Append every nth image defined by pano_rate  
+            # if self.count % self.pano_rate == 0:
+
+            #     self.imgs.append(warp_dst)
+
+            # Make panorama every n processed images
+            # n = 2
+            # if self.count % (self.pano_rate * n) == 0:
+            #     result = cv2.addWeighted(self.imgs[0], 0.5, self.imgs[1], 0.5, 0)
+                #cv2.imshow("Translated and Overlaid Images", result)
+
+                #cv2.waitKey(3000)      
+                # status, pano = self.stitcher.stitch(self.imgs)
+                # self.imgs = []
+                # if status != cv2.Stitcher_OK:
+                #     print("Can't stitch images, error code = %d" % status)
+                #     return
+
+                # cv2.imshow("panorama", pano)
+                
+                # package_path = rospkg.RosPack().get_path('uvo')  
+                # filename =f"{package_path}/media/i2mage_{ self.count//(n*self.pano_rate):02d}.png"  
+                # cv2.imwrite(filename, pano)
+                # print( self.count//(n*self.pano_rate) ," - stitching completed successfully. Image saved!")
+                # cv2.waitKey(10)
+        
+            
     
         # Save information about previous frame
         self.last_kp = keypoints
@@ -69,8 +139,10 @@ class uvonode:
         try:
             # Convert the ROS Image message to OpenCV format
             cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
-        
+
             self.process_image(cv_image)
+            self.count+=1        
+
 
 
         except Exception as e:
@@ -117,6 +189,16 @@ class uvonode:
         self.imageTopic = rospy.get_param("/computed_pose_topic", "/computed_pose")
 
         self.pub = rospy.Publisher("/computed_pose", Pose, queue_size=1)
+
+
+        #![stitching]
+        self.stitcher = cv2.Stitcher.create(mode=1) # SCANS
+        self.imgs = []
+        self.count = 0
+        self.pano_rate = 10
+        self.M = None
+        self.pano = None
+
  
 
 
